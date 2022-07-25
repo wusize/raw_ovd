ec29dfa8538b207618e35e66047554235c48f5b8
吴思泽 <wusize@wusizes-MacBook-Pro.local>
2022-07-26 00:22:04 +0800

    bug

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ GPUS=${GPUS:-8}
 SLEEP=${SLEEP:-0h}
 ARGS=${ARGS:-""}
 sudo sleep ${GPUS}
-sudo python train_net.py --num-gpus ${GPUS} --config-files ${CONFIG} ${ARGS}
+sudo python train_net.py --num-gpus ${GPUS} --config-file ${CONFIG} ${ARGS}
