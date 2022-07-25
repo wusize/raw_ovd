7fe7e33679b36d18744f28009e1f91e1d869afc9
吴思泽 <wusize@wusizes-MacBook-Pro.local>
2022-07-25 22:04:29 +0800

    download_only

--- tools/download_cc_lvis.py
+++ tools/download_cc_lvis.py
@@ -1,8 +1,6 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 import json
 import argparse
-from PIL import Image
-import numpy as np
 import os
 import threading
 import math
@@ -26,27 +24,12 @@ if __name__ == '__main__':
     num_threads = math.ceil(num_images / num_images_per_thread)
 
     def _read_images(start_id):
-        valid_images = []
         end_id = min(start_id + num_images_per_thread, len(images))
         for i in range(start_id, end_id):
             img_info = images[i]
             os.system(
-                f"wget {img_info['path']} -t 1 -T 4 -O {args.save_image_path}/{img_info['file_name']}")
-            try:
-                img = Image.open(
-                    open('{}/{}.jpg'.format(args.save_image_path, i + 1), "rb"))
-                img = np.asarray(img.convert("RGB"))
-                h, w = img.shape[:2]
-            except:
-                continue
-            img_info.update(height=h, width=w)
-            valid_images.append(img_info)
+                f"wget {img_info['path']} -t 1 -T 10 -O {args.save_image_path}/{img_info['file_name']}")
 
-        data = {'images': valid_images}
-        out = args.ann.replace('.json', f'_{start_id}.json')
-        print('Saving to', out)
-        with open(out, 'w') as f:
-            json.dump(data, f)
     threads = []
     for thr in range(num_threads):
         t = threading.Thread(target=_read_images, name='read_images',
@@ -55,16 +38,3 @@ if __name__ == '__main__':
         t.start()
     for t in threads:
         t.join()
-    del images
-    valid_images = []
-    for thr in range(num_threads):
-        start_id = thr * num_images_per_thread
-        out = args.ann.replace('.json', f'_{start_id}.json')
-        with open(out, 'r') as f:
-            part_data = json.load(f)
-        valid_images.extend(part_data['images'])
-        os.system(f'rm {out}')
-    images_info['images'] = valid_images
-
-    with open(args.ann.replace('.json', f'_valid.json'), 'w') as f:
-        json.dump(images_info, f, indent=4, sort_keys=True)
