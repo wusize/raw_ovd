e02a5c68929944af2963a5393afa8191a164329b
wusize <wusize@sensetime.com>
2022-08-21 21:15:06 +0800

    'dc5'

--- detic/modeling/roi_heads/disentangle_context_modelling.py
+++ detic/modeling/roi_heads/disentangle_context_modelling.py
@@ -2,8 +2,8 @@ from .context_modelling import ContextModelling
 
 
 class ContextModellingV2(ContextModelling):
-    def get_loss(self, group_infos, clip_images, clip_model, image_info, roi_head, features):
+    def get_loss(self, group_infos, clip_images, clip_model, image_info, roi_head, features, *args, **kwargs):
         sampled_instances = [g['sampled_instances'] for g in group_infos]
-        predictions = roi_head.get_pseudo_words(sampled_instances, features)
+        predictions = roi_head.get_pseudo_words(sampled_instances, features, *args, **kwargs)
         return super(ContextModellingV2, self).get_loss(group_infos, predictions,
                                                         clip_images, clip_model, image_info)
