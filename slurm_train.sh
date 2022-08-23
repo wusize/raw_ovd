053256e1fe78787718c296c1a538c6c0a745dab0
wusize <wusize@sensetime.com>
2022-08-23 15:39:16 +0800

    'sbatch'

--- slurm_train.sh
+++ slurm_train.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-#SBATCH -p YOUR_PARTITION
+#SBATCH -p pat_dev
 #SBATCH --nodes=2
 #SBATCH --gres=gpu:8
-#SBATCH --gpus-per-node=8
-#SBATCH --ntasks-per-node=8
+#SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=40
 #SBATCH --time 4320
 #SBATCH -o "output/slurm-%j.out"
 
-srun multi-node_run.sh $@
+srun bash multi-node_run.sh $@
