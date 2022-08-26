2396b2fdce9e6fa3d26bddcf03c97a7ca207ee8a
wusize <wusize@sensetime.com>
2022-08-26 17:15:18 +0800

    'rpn'

---
+++ detic/modeling/meta_arch/proposal_network.py
@@ -0,0 +1,50 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+import logging
+from torch import nn
+from detectron2.structures import ImageList
+from detectron2.utils.logger import log_first_n
+
+from detectron2.modeling.meta_arch.build import META_ARCH_REGISTRY
+
+__all__ = ["CustomProposalNetwork"]
+
+
+@META_ARCH_REGISTRY.register()
+class CustomProposalNetwork(nn.Module):
+    def forward(self, batched_inputs):
+        """
+        Args:
+            Same as in :class:`GeneralizedRCNN.forward`
+
+        Returns:
+            list[dict]:
+                Each dict is the output for one input image.
+                The dict contains one key "proposals" whose value is a
+                :class:`Instances` with keys "proposal_boxes" and "objectness_logits".
+        """
+        images = [x["image"].to(self.device) for x in batched_inputs]
+        images = [(x - self.pixel_mean) / self.pixel_std for x in images]
+        images = ImageList.from_tensors(images, self.backbone.size_divisibility)
+        features = self.backbone(images.tensor)
+
+        if "instances" in batched_inputs[0]:
+            gt_instances = [x["instances"].to(self.device) for x in batched_inputs]
+        elif "targets" in batched_inputs[0]:
+            log_first_n(
+                logging.WARN, "'targets' in the model inputs is now renamed to 'instances'!", n=10
+            )
+            gt_instances = [x["targets"].to(self.device) for x in batched_inputs]
+        else:
+            gt_instances = None
+        proposals, proposal_losses = self.proposal_generator(images, features, gt_instances)
+        # In training, the proposals are not useful at all but we generate them anyway.
+        # This makes RPN-only models about 5% slower.
+        if self.training:
+            return proposal_losses
+
+        processed_results = []
+        for r, input_per_image, image_size in zip(
+            proposals, batched_inputs, images.image_sizes
+        ):
+            processed_results.append({"proposals": r})
+        return processed_results
