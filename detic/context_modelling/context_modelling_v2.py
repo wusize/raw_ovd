647a0c78aa402ddf5611cc0e95b21a24b8dee067
吴思泽 <wusize@wusizes-MacBook-Pro.local>
2022-08-28 00:43:51 +0800

    add_to_topk

--- detic/context_modelling/context_modelling_v2.py
+++ detic/context_modelling/context_modelling_v2.py
@@ -6,6 +6,9 @@ from detectron2.modeling.proposal_generator.proposal_utils import \
     add_ground_truth_to_proposals_single_image
 from detectron2.structures.masks import PolygonMasks
 from .context_modelling import ContextModelling
+from detic.data.datasets.coco_zeroshot import categories_unseen
+
+novel_cat_ids = [cat['id'] for cat in categories_unseen]
 
 
 class ContextModellingV2(ContextModelling):
@@ -13,48 +16,33 @@ class ContextModellingV2(ContextModelling):
         super(ContextModellingV2, self).__init__(cfg, num_words, word_embed_dim, word_dropout, sigmoid)
         pr_coco = COCO(self.cfg.PROPOSALS)
         proposals = {}
+        is_unseen = []
         for img_id, anns in pr_coco.imgToAnns.items():
             boxes = torch.tensor([ann['bbox'] for ann in anns])
+            is_unseen.extend([1.0 if ann['category_id'] in novel_cat_ids else 0.0 for ann in anns])
             boxes[:, 2:] = boxes[:, :2] + boxes[:, 2:]
             img_info = pr_coco.imgs[img_id]
             image_size = (img_info['height'], img_info['width'])
-            proposals[img_id] = Instances(image_size=image_size, proposal_boxes=Boxes(boxes))
+            proposals[img_id] = Instances(image_size=image_size, gt_boxes=Boxes(boxes))
+        print(f'Possible novel instances: {sum(is_unseen) / len(is_unseen)}', flush=True)
 
         self.proposals = proposals
 
-    def _get_proposals(self, image_id, gt_instances):
-        tar_image_size = gt_instances.image_size
-        device = gt_instances.gt_boxes.device
+    def _get_proposals(self, image_id, topk_proposals):
+        tar_image_size = topk_proposals.image_size
+        device = topk_proposals.proposal_boxes.device
         if image_id in self.proposals:
-            proposals = self.proposals[image_id]
-            ori_image_size = proposals.image_size
-            boxes = proposals.proposal_boxes
+            gt_proposals = self.proposals[image_id]
+            ori_image_size = gt_proposals.image_size
+            boxes = gt_proposals.gt_boxes
             boxes.scale(scale_x=tar_image_size[1] / ori_image_size[1],
                         scale_y=tar_image_size[0] / ori_image_size[0])
-            num_boxes = len(boxes)
-            proposals = Instances(image_size=tar_image_size,
-                                  proposal_boxes=boxes,
-                                  objectness_logits=torch.ones(num_boxes)).to(device)
-        else:
-            proposals = Instances(image_size=tar_image_size,
-                                  proposal_boxes=Boxes(torch.zeros(0, 4)),
-                                  objectness_logits=torch.ones(0)).to(device)
+            gt_proposals = Instances(image_size=tar_image_size,
+                                     gt_boxes=boxes).to(device)
+            topk_proposals = add_ground_truth_to_proposals_single_image(gt_proposals, topk_proposals)
 
-        if self.cfg.ADD_GT:
-            proposals = add_ground_truth_to_proposals_single_image(gt=gt_instances,
-                                                                   proposals=proposals)
-        num_proposals = len(proposals)
-        proposals.set('gt_classes', -torch.ones(num_proposals, device=device, dtype=torch.int64))
-        proposals.set('gt_boxes', Boxes(torch.zeros(num_proposals, 4, device=device)))
-        proposals.set('sample_types', -torch.ones(num_proposals, device=device).int())
-
-        return proposals
+        return topk_proposals
 
     # TODO: input topk proposals
-    def sample(self, image_id, gt_instances, mask_on=False):
-        proposals = self._get_proposals(image_id, gt_instances)
-        if mask_on:
-            proposals.set('gt_masks',
-                          PolygonMasks([[np.asarray([0.0, 0.0, 0.0, 1.0, 1.0, 0.0])]] * len(proposals)))
-
-        return self.sample_on_topk(proposals, mask_on)
+    def sample(self, image_id, proposals, mask_on=False):
+        return super(ContextModellingV2, self).sample(self._get_proposals(image_id, proposals), mask_on)
