335911b9afcbfcb69727d6d186731581c24448c2
wusize <wusize@sensetime.com>
2022-08-31 17:14:56 +0800

    'multi_level'

---
+++ detic/context_modelling/context_modelling_v4.py
@@ -0,0 +1,81 @@
+import torch
+import numpy as np
+from .utils import multi_apply
+from .context_modelling import ContextModelling
+from detectron2.structures import Instances, Boxes, PolygonMasks
+from detectron2.modeling.poolers import assign_boxes_to_levels
+
+
+class ContextModellingV4(ContextModelling):
+    # TODO: multi-level sampling strategy
+    # preprocess topk proposals
+    @staticmethod
+    def _deal_with_empty_proposals(empty_proposals, mask_on):
+        h, w = empty_proposals.image_size
+        device = empty_proposals.proposal_boxes.device
+
+        image_box = torch.tensor([0.0, 0.0, w - 1.0, h - 1.0], device=device)
+        topk_proposals = Instances(image_size=empty_proposals.image_size,
+                                   proposal_boxes=Boxes(image_box.view(-1, 4)),
+                                   objectness_logits=-torch.ones(1, device=device),
+                                   gt_classes=-torch.ones(1, device=device, dtype=torch.int64),
+                                   gt_boxes=Boxes(torch.zeros(1, 4, device=device)),
+                                   sample_types=-torch.ones(1, device=device).int())
+        if mask_on:
+            topk_proposals.set('gt_masks',
+                               PolygonMasks([[np.asarray([0.0, 0.0, 0.0, 1.0, 1.0, 0.0]), ], ]))
+
+        return topk_proposals
+
+    def _checkboard_sampling(self, topk_proposals, mask_on=False):
+        if not self.checkboard_cfg.ENABLE:
+            return topk_proposals[:0], None
+        device = topk_proposals.proposal_boxes.device
+        if len(topk_proposals) == 0:
+            topk_proposals = self._deal_with_empty_proposals(topk_proposals, mask_on)
+
+        num_levels = len(self.checkboard_cfg.AREA_THR)
+
+        level_assignment = assign_boxes_to_levels([topk_proposals.proposal_boxes],
+                                                  0, num_levels - 1,
+                                                  self.checkboard_cfg.AREA_THR[-2], num_levels - 2)
+        # TODO: sample at each level
+        multi_level_instances = []
+        for i in range(num_levels):
+            lvl_proposals = topk_proposals[level_assignment == i]
+            if len(lvl_proposals) == 0:
+                continue
+            shape_ratio_thr = self.checkboard_cfg.SHAPE_RATIO_THR[i]
+            area_ratio_thr = self.checkboard_cfg.AREA_RATIO_THR
+            objectness_thr = self.checkboard_cfg.OBJECTNESS_THR[i]
+            nms_thr = self.checkboard_cfg.NMS_THR[i]
+            lvl_proposals = self.preprocess_proposals(lvl_proposals,
+                                                      shape_ratio_thr,
+                                                      area_ratio_thr,
+                                                      objectness_thr,
+                                                      nms_thr)
+            lvl_proposals.sample_types[:] = 1    # clip_kd_samples: 1
+            multi_level_instances.append(lvl_proposals)
+        sampled_proposals = Instances.cat(multi_level_instances)
+        func = self.checkboard_sampling.sample
+        boxes = sampled_proposals.proposal_boxes.tensor.tolist()
+        groups_per_proposal, normed_boxes, spanned_boxes, box_ids = \
+            multi_apply(func, boxes,
+                        [sampled_proposals.image_size] * len(sampled_proposals))
+        new_boxes = torch.cat([c for p in groups_per_proposal
+                               for g in p for c in g], dim=0).to(device)
+        num_added = len(new_boxes)
+        added_instances = Instances(image_size=sampled_proposals.image_size,
+                                    proposal_boxes=Boxes(new_boxes),
+                                    objectness_logits=-torch.ones(num_added, device=device),
+                                    gt_classes=-torch.ones(num_added, device=device,
+                                                           dtype=torch.int64),
+                                    gt_boxes=Boxes(torch.zeros(num_added, 4, device=device)),
+                                    sample_types=torch.ones(num_added, device=device).int())   # clip_kd: 1
+        if mask_on:
+            added_instances.set('gt_masks',
+                                PolygonMasks([[np.asarray([0.0, 0.0, 0.0, 1.0, 1.0, 0.0])]] * num_added))
+
+        return added_instances, dict(normed_boxes=normed_boxes,
+                                     spanned_boxes=spanned_boxes,
+                                     box_ids=box_ids)
---
+++ detic/modeling/roi_heads/standard_roi_heads_v4.py
@@ -0,0 +1,234 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+from detectron2.structures.instances import Instances
+from detectron2.structures.boxes import Boxes
+import torch
+import numpy as np
+from detectron2.config import configurable
+from detectron2.modeling.roi_heads.roi_heads import ROI_HEADS_REGISTRY, StandardROIHeads
+from .detic_fast_rcnn import DeticFastRCNNOutputLayers
+from torch.nn import functional as F
+from detectron2.utils.events import get_event_storage
+from detectron2.modeling.proposal_generator.proposal_utils \
+    import add_ground_truth_to_proposals
+from detectron2.structures import pairwise_iou
+from detic.context_modelling.context_modelling_v4 import ContextModellingV4 as ContextModelling
+from time import time
+from torchvision.ops import box_iou
+
+
+@ROI_HEADS_REGISTRY.register()
+class CustomStandardROIHeadsV4(StandardROIHeads):
+    @configurable
+    def __init__(self, **kwargs):
+        cfg = kwargs.pop('cfg')
+        super().__init__(**kwargs)
+        self.ws_num_props = cfg.MODEL.ROI_BOX_HEAD.WS_NUM_PROPS
+        self.image_box_size = cfg.MODEL.ROI_BOX_HEAD.IMAGE_BOX_SIZE
+        self.box_predictor = DeticFastRCNNOutputLayers(
+            cfg,  self.box_head.output_shape
+        )
+
+        self.context_modeling_cfg = cfg.CONTEXT_MODELLING_V4
+        self.cfg = cfg
+
+        self.context_modeling = ContextModelling(self.context_modeling_cfg,
+                                                 num_words=self.box_predictor.num_words,
+                                                 word_embed_dim=self.box_predictor.word_embed_dim,
+                                                 word_dropout=self.box_predictor.word_dropout)
+
+    @classmethod
+    def from_config(cls, cfg, input_shape):
+        ret = super().from_config(cfg, input_shape)
+        ret['cfg'] = cfg
+        return ret
+
+    def _forward_box(self, features, proposals,
+                     clip_images=None, image_info=None,
+                     resized_image_info=None, group_infos=None, targets=None):
+        features = [features[f] for f in self.box_in_features]
+        box_features = self.box_pooler(features, [x.proposal_boxes for x in proposals])
+        box_features = self.box_head(box_features)
+
+        if self.training:
+            losses = dict()
+            storage = get_event_storage()
+            tik = time()
+            predictions = self._box_forward_train(box_features, proposals, targets)
+            losses.update(self.box_predictor.losses(predictions,
+                                                    [p[p.sample_types == 0] for p in proposals]))
+            tok = time()
+            # print('detector loss:', tok - tik)
+            storage.put_scalar("time/detector_forward", np.float32(tok - tik))
+
+            # TODO contrastive learning
+            if self.context_modeling_cfg.ENABLE:
+                losses.update(self.context_modeling.get_loss(group_infos,
+                                                             predictions, clip_images,
+                                                             self.box_predictor.clip, image_info))
+                storage.put_scalar("time/contrast_learning", np.float32(time() - tok))
+
+            if self.cfg.MODEL.WITH_IMAGE_LABELS:
+                loss = self.image_label_loss(resized_image_info)
+                if loss is None:
+                    loss = list(losses.values())[0] * 0.0
+                losses.update(image_label_loss=loss)
+
+            if self.train_on_pred_boxes:
+                with torch.no_grad():
+                    pred_boxes = self.box_predictor.predict_boxes_for_gt_classes(
+                        predictions, proposals
+                    )
+                    for proposals_per_image, pred_boxes_per_image in zip(proposals, pred_boxes):
+                        proposals_per_image.proposal_boxes = Boxes(pred_boxes_per_image)
+
+            return losses
+        else:
+            predictions = self.box_predictor(box_features)
+            del box_features
+            pred_instances, _ = self.box_predictor.inference(predictions, proposals)
+            return pred_instances
+
+    def forward(self, images, features, proposals, targets=None,
+                ann_types=None, clip_images=None, image_info=None,
+                resized_image_info=None):
+        '''
+        enable debug and image labels
+        '''
+
+        del images
+        if self.training:
+            proposals, group_infos = self.label_and_sample_proposals(
+                proposals, targets, ann_types=ann_types)
+            losses = self._forward_box(features, proposals, clip_images, image_info,
+                                       resized_image_info, group_infos, targets=targets)
+            # Usually the original proposals used by the box head are used by the mask, keypoint
+            # heads. But when `self.train_on_pred_boxes is True`, proposals will contain boxes
+            # predicted by the box head.
+            losses.update(self._forward_mask(features, proposals))
+            losses.update(self._forward_keypoint(features, proposals))
+            return proposals, losses
+        else:
+            pred_instances = self._forward_box(features, proposals)
+            # During inference cascaded prediction is used: the mask and keypoints heads are only
+            # applied to the top scoring box detections.
+            pred_instances = self.forward_with_given_boxes(features, pred_instances)
+            return pred_instances, {}
+
+    @torch.no_grad()
+    def label_and_sample_proposals(self, proposals, targets, ann_types):
+        if self.proposal_append_gt:
+            proposals = add_ground_truth_to_proposals(targets, proposals)
+
+        proposals_with_gt = []
+
+        num_fg_samples = []
+        num_bg_samples = []
+        group_infos = []
+        for proposals_per_image, targets_per_image, ann_type in zip(proposals, targets, ann_types):
+            has_gt = len(targets_per_image) > 0
+            match_quality_matrix = pairwise_iou(
+                targets_per_image.gt_boxes, proposals_per_image.proposal_boxes
+            )
+            matched_idxs, matched_labels = self.proposal_matcher(match_quality_matrix)
+            sampled_idxs, gt_classes = self._sample_proposals(
+                matched_idxs, matched_labels, targets_per_image.gt_classes
+            )
+            added_instances, group_info = self.context_modeling.sample(proposals_per_image, self.mask_on)
+            group_infos.append(group_info)
+            # sample type: -1 for topk; 0 for det; 1 for clip-img; 2 for caption
+
+            # Set target attributes of the sampled proposals:
+            proposals_per_image = proposals_per_image[sampled_idxs]
+            proposals_per_image.gt_classes = gt_classes
+
+            if has_gt:
+                sampled_targets = matched_idxs[sampled_idxs]
+                for (trg_name, trg_value) in targets_per_image.get_fields().items():
+                    if trg_name.startswith("gt_") and not proposals_per_image.has(trg_name):
+                        proposals_per_image.set(trg_name, trg_value[sampled_targets])
+            proposals_per_image.set('sample_types',
+                                    torch.zeros_like(proposals_per_image.gt_classes).int())
+            if ann_type == 'only_caption':
+                proposals_per_image = proposals_per_image[:0]
+            proposals_per_image = Instances.cat([proposals_per_image, added_instances])
+            num_bg_samples.append((gt_classes == self.num_classes).sum().item())
+            num_fg_samples.append(gt_classes.numel() - num_bg_samples[-1])
+            proposals_with_gt.append(proposals_per_image)
+
+        # Log the number of fg/bg samples that are selected for training ROI heads
+        storage = get_event_storage()
+        storage.put_scalar("roi_head/num_fg_samples", np.mean(num_fg_samples))
+        storage.put_scalar("roi_head/num_bg_samples", np.mean(num_bg_samples))
+
+        return proposals_with_gt, group_infos
+
+    def _box_forward_train(self, box_features, proposals, targets):
+        input_box_features = self.box_predictor.pre_forward(box_features)
+        del box_features
+        pseudo_words_ = self.box_predictor.pred_words(input_box_features)
+        sample_types = torch.cat([p.sample_types for p in proposals], dim=0)
+
+        # TODO: detach base words for context modelling
+        if self.context_modeling_cfg.DETACH_BASE:
+            iou_with_gt_base = [box_iou(p.proposal_boxes.tensor,
+                                        t.gt_boxes.tensor).max(-1).values
+                                if len(t) > 0
+                                else
+                                p.proposal_boxes.tensor.new_zeros(len(p))
+                                for p, t in zip(proposals, targets)
+                                ]
+            iou_with_gt_base = torch.cat(iou_with_gt_base)
+            pseudo_words = torch.zeros_like(pseudo_words_)
+            need_detach = torch.logical_and(sample_types > 0,
+                                            iou_with_gt_base > self.context_modeling_cfg.DETACH_THR)
+            pseudo_words[need_detach] = pseudo_words_[need_detach].detach()
+            pseudo_words[need_detach.logical_not()] = pseudo_words_[need_detach.logical_not()]
+        else:
+            pseudo_words = pseudo_words_
+
+        storage = get_event_storage()
+        tik = time()
+        scores = self.box_predictor.pred_cls_score(pseudo_words[sample_types == 0])
+        storage.put_scalar('time/pred_cls', time() - tik)
+        proposal_deltas = self.box_predictor.bbox_pred(input_box_features[sample_types == 0])
+        del input_box_features
+        predictions = dict(kd_pseudo_words=pseudo_words[sample_types == 1],
+                           caption_pseudo_words=pseudo_words[sample_types == 2],
+                           scores=scores,
+                           proposal_deltas=proposal_deltas)
+
+        return predictions
+
+    def image_label_loss(self, resized_image_info):
+        proposals = resized_image_info['proposals']
+        num_imgs = len(proposals)
+        if num_imgs == 0:
+            return None
+        proposals = [p[:self.cfg.MODEL.ROI_BOX_HEAD.WS_NUM_PROPS] for p in proposals]
+        image_labels = resized_image_info['image_labels']
+        max_size_proposals = []
+        for p in proposals:
+            assert len(p) > 0
+            areas = p.proposal_boxes.area()
+            idx = areas.argmax().item()
+            max_size_proposals.append(p[idx:idx + 1])
+        features = resized_image_info['features']
+        features = [features[f] for f in self.box_in_features]
+        box_features = self.box_pooler(features, [x.proposal_boxes for x in max_size_proposals])
+        box_features = self.box_head(box_features)
+        box_features = self.box_predictor.pre_forward(box_features)
+        pseudo_words = self.box_predictor.pred_words(box_features)  # Nx1024 -> Nx4x512
+        scores = self.box_predictor.pred_cls_score(pseudo_words)[..., :-1]  # discard bg
+        targets = torch.zeros_like(scores)
+        loss_weights = torch.ones_like(scores)
+        for i in range(num_imgs):
+            targets[i, image_labels[i]] = 1.0
+            loss_weights[i, image_labels[i]] = self.cfg.MODEL.ROI_BOX_HEAD.IMAGE_POS_WEIGHT
+
+        loss = F.binary_cross_entropy_with_logits(scores, targets, reduction='none')
+        loss = (loss * loss_weights).sum() / (loss_weights.sum() + 1e-12)
+
+        if loss > 100.0:
+            loss = loss * 0.0
+
+        return loss * self.cfg.MODEL.ROI_BOX_HEAD.IMAGE_LOSS_WEIGHT
