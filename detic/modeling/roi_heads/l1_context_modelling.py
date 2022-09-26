5728532797703257ae11b51f415adb4c1f2d317d
吴思泽 <wusize@wusizes-MacBook-Pro.local>
2022-09-26 23:32:37 +0800

    l1

---
+++ detic/modeling/roi_heads/l1_context_modelling.py
@@ -0,0 +1,120 @@
+import torch
+import torch.nn.functional as F
+from torchvision.ops import roi_align
+from .utils import (multi_apply,
+                    bbox_xyxy_to_cxcywh)
+from detectron2.utils.events import get_event_storage
+import numpy as np
+from time import time
+from torch.cuda.amp import autocast
+
+from .context_modelling import ContextModelling, process_single_image_groups
+
+
+class L1ContextModelling(ContextModelling):
+
+    def kd_clip_contrast(self,
+                         group_info,
+                         predictions, clip_images,
+                         clip_model,
+                         image_info=None):
+        image_ids = [im['image_id'] for im in image_info]
+        pseudo_words = predictions.pop('kd_pseudo_words')
+        device = pseudo_words.device
+        storage = get_event_storage()
+        storage.put_scalar("num_proposals/contrast_proposals", np.float32(pseudo_words.shape[0]))
+        # Note: perms = seq
+        normed_boxes, spanned_boxes, origin_split, group_split, preds_split_by_perms,\
+            seqs_split_split_by_origin, seqs_split_by_group = \
+            multi_apply(process_single_image_groups, group_info, device=device)
+        positions = bbox_xyxy_to_cxcywh(torch.cat(normed_boxes, dim=0))
+        position_embeddings = self.positional_embed(positions)
+        pseudo_words = pseudo_words + position_embeddings
+        word_masks = self._drop_word(pseudo_words)
+        start_id = 0
+        seq_ids = []
+        for g in group_info:
+            seq_ids_ = g['seq_ids']
+            for seq_id in seq_ids_:
+                seq_ids.append(seq_id + start_id)
+            start_id += (max(seq_ids_) + 1)
+        normed_boxes_split_by_perms = [normed_boxes_.split(preds_split_by_perms_, dim=0)
+                                       for normed_boxes_, preds_split_by_perms_
+                                       in zip(normed_boxes, preds_split_by_perms)]
+        # torch.cat(normed_boxes).split(preds_split_by_perms, dim=0)
+        preds_split_by_perms = [p for b in preds_split_by_perms for p in b]
+        word_sequences = pseudo_words.split(preds_split_by_perms, dim=0)
+        word_masks = word_masks.split(preds_split_by_perms, dim=0)
+        word_sequences = [seq.flatten(0, 1)[wm.flatten(0, 1)] for seq, wm in zip(word_sequences, word_masks)]
+        context_length = max([seq.shape[0] for seq in word_sequences])
+        tok = time()
+        clip_model.eval()
+        with autocast():
+            # TODO: get local image tokens
+            pseudo_text, end_token_ids = clip_model.prepare_pseudo_text(
+                word_sequences,
+                context_length=context_length + 2)  # add start and stop token
+            clip_text_features, clip_word_tokens = \
+                clip_model.encode_pseudo_text(pseudo_text, end_token_ids,
+                                              text_pe=True, normalize=True,
+                                              return_word_tokens=True)
+            clip_text_features = clip_text_features.float()
+            clip_image_features, clip_image_tokens = self._bbox_clip_image(spanned_boxes, clip_images,
+                                                                           seqs_split_by_group,
+                                                                           normed_boxes_split_by_perms,
+                                                                           clip_model)
+        tik = time()
+        storage.put_scalar("contrast_learning_time/clip_model_forward",
+                           np.float32(tik-tok))
+        num_queries = clip_text_features.shape[0]
+        assert clip_image_features.shape[0] == num_queries
+
+        contrast_loss_l1 = torch.abs(clip_text_features - clip_image_features).mean()
+
+        # mask same synced_img
+        img_ids = [torch.tensor(sum(b) * [img_id])
+                   for b, img_id in zip(seqs_split_split_by_origin,
+                                        image_ids)]
+        img_ids = torch.cat(img_ids).to(device)
+
+        losses = dict(contrast_loss_l1=contrast_loss_l1 * self.cfg.CONTRAST_LOSS_WEIGHT)
+        # Enqueue
+        queues_update = dict(clip_text_features=torch.cat([clip_text_features,
+                                                      img_ids.view(-1, 1)], dim=-1).detach(),
+                             clip_image_features=torch.cat([clip_image_features,
+                                                      img_ids.view(-1, 1)], dim=-1).detach()
+                             )
+
+        if self.checkboard_cfg.LOCAL_CORRESPONDENCE:
+            tik = time()
+            preds_split_by_batch = [n.shape[0] for n in normed_boxes]
+            img_ids = [torch.tensor(b * [img_id])
+                       for b, img_id in zip(preds_split_by_batch,
+                                            image_ids)]
+            img_ids = torch.cat(img_ids).to(device)
+            normed_boxes = torch.cat(normed_boxes, dim=0).split(preds_split_by_perms, dim=0)
+            clip_patch_features = F.normalize(roi_align(
+                clip_image_tokens, normed_boxes, (1, 1),
+                float(clip_image_tokens.shape[-1]), 2, True)[..., 0, 0], dim=-1)
+            num_words_per_pred = [wm.sum(-1).tolist() for wm in word_masks]
+            clip_word_features = [tk.split(spl) for (tk, spl)
+                                  in zip(clip_word_tokens, num_words_per_pred)]
+            clip_word_features = F.normalize(torch.stack([feat.mean(0).float()
+                                                          for feats in clip_word_features
+                                                          for feat in feats], dim=0), dim=-1)
+            tok = time()
+            storage.put_scalar("contrast_learning_time/prepare_dense_features",
+                               np.float32(tok - tik))
+
+            num_queries = clip_patch_features.shape[0]
+            assert num_queries == clip_word_features.shape[0]
+
+            token_loss_l1 = torch.abs(clip_word_features - clip_patch_features).mean()
+
+            losses.update(token_loss_l1=token_loss_l1 * self.cfg.TOKEN_LOSS_WEIGHT)
+
+            queues_update.update(clip_word_features=torch.cat([clip_word_features,
+                                                               img_ids.view(-1, 1)], dim=-1).detach(),
+                                 clip_patch_features=torch.cat([clip_patch_features,
+                                                                img_ids.view(-1, 1)], dim=-1).detach())
+        return losses, queues_update
---
+++ detic/modeling/roi_heads/res5_roi_heads_l1.py
@@ -0,0 +1,156 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+from detectron2.structures.instances import Instances
+import torch
+import numpy as np
+from detectron2.config import configurable
+from detectron2.layers import ShapeSpec
+from detectron2.modeling.roi_heads.roi_heads import ROI_HEADS_REGISTRY, Res5ROIHeads
+from .detic_fast_rcnn import DeticFastRCNNOutputLayers
+from detectron2.utils.events import get_event_storage
+from detectron2.modeling.proposal_generator.proposal_utils \
+    import add_ground_truth_to_proposals
+from detectron2.structures import pairwise_iou
+from detic.modeling.roi_heads.l1_context_modelling import L1ContextModelling as ContextModelling
+from time import time
+
+
+@ROI_HEADS_REGISTRY.register()
+class CustomRes5ROIHeadsL1(Res5ROIHeads):
+    @configurable
+    def __init__(self, **kwargs):
+        cfg = kwargs.pop('cfg')
+        super().__init__(**kwargs)
+        stage_channel_factor = 2 ** 3
+        out_channels = cfg.MODEL.RESNETS.RES2_OUT_CHANNELS * stage_channel_factor
+
+        self.ws_num_props = cfg.MODEL.ROI_BOX_HEAD.WS_NUM_PROPS
+        self.add_image_box = cfg.MODEL.ROI_BOX_HEAD.ADD_IMAGE_BOX
+        self.add_feature_to_prop = cfg.MODEL.ROI_BOX_HEAD.ADD_FEATURE_TO_PROP
+        self.image_box_size = cfg.MODEL.ROI_BOX_HEAD.IMAGE_BOX_SIZE
+        self.box_predictor = DeticFastRCNNOutputLayers(
+            cfg, ShapeSpec(channels=out_channels, height=1, width=1)
+        )
+
+        self.context_modeling_cfg = cfg.CONTEXT_MODELLING
+        self.cfg = cfg
+
+        self.context_modeling = ContextModelling(self.context_modeling_cfg,
+                                                 num_words=self.box_predictor.num_words,
+                                                 word_embed_dim=self.box_predictor.word_embed_dim,
+                                                 word_dropout=self.box_predictor.word_dropout)
+
+    @classmethod
+    def from_config(cls, cfg, input_shape):
+        ret = super().from_config(cfg, input_shape)
+        ret['cfg'] = cfg
+        return ret
+
+    def forward(self, images, features, proposals, targets=None,
+                ann_types=None, clip_images=None, image_info=None,
+                resized_image_info=None):
+        '''
+        enable debug and image labels
+        '''
+        del images
+        if self.training:
+            proposals, group_infos = self.label_and_sample_proposals(
+                proposals, targets, ann_types=ann_types)
+
+        proposal_boxes = [x.proposal_boxes for x in proposals]
+        box_features = self._shared_roi_transform(
+            [features[f] for f in self.in_features], proposal_boxes
+        )
+        
+        if self.add_feature_to_prop:
+            feats_per_image = box_features.mean(dim=[2, 3]).split(
+                [len(p) for p in proposals], dim=0)
+            for feat, p in zip(feats_per_image, proposals):
+                p.feat = feat
+
+        if self.training:
+            del features
+            storage = get_event_storage()
+            tik = time()
+            predictions = self._box_forward_train(box_features, proposals)
+            losses = self.box_predictor.losses(predictions,
+                                               [p[p.sample_types == 0] for p in proposals])
+            tok = time()
+            # print('detector loss:', tok - tik)
+            storage.put_scalar("time/detector_forward", np.float32(tok - tik))
+
+            # TODO contrastive learning
+            if self.context_modeling_cfg.ENABLE:
+                losses.update(self.context_modeling.get_loss(group_infos,
+                                                             predictions, clip_images,
+                                                             self.box_predictor.clip, image_info))
+                storage.put_scalar("time/contrast_learning", np.float32(time() - tok))
+
+            return proposals, losses
+        else:
+            predictions = self.box_predictor(
+                box_features.mean(dim=[2, 3]))
+            pred_instances, _ = self.box_predictor.inference(predictions, proposals)
+            pred_instances = self.forward_with_given_boxes(features, pred_instances)
+            return pred_instances, {}
+
+    @torch.no_grad()
+    def label_and_sample_proposals(self, proposals, targets, ann_types):
+        if self.proposal_append_gt:
+            proposals = add_ground_truth_to_proposals(targets, proposals)
+
+        proposals_with_gt = []
+
+        num_fg_samples = []
+        num_bg_samples = []
+        group_infos = []
+        for proposals_per_image, targets_per_image, ann_type in zip(proposals, targets, ann_types):
+            has_gt = len(targets_per_image) > 0
+            match_quality_matrix = pairwise_iou(
+                targets_per_image.gt_boxes, proposals_per_image.proposal_boxes
+            )
+            matched_idxs, matched_labels = self.proposal_matcher(match_quality_matrix)
+            sampled_idxs, gt_classes = self._sample_proposals(
+                matched_idxs, matched_labels, targets_per_image.gt_classes
+            )
+            added_instances, group_info = self.context_modeling.sample(proposals_per_image)
+            group_infos.append(group_info)
+            # sample type: -1 for topk; 0 for det; 1 for clip-img; 2 for caption
+
+            # Set target attributes of the sampled proposals:
+            proposals_per_image = proposals_per_image[sampled_idxs]
+            proposals_per_image.gt_classes = gt_classes
+
+            if has_gt:
+                sampled_targets = matched_idxs[sampled_idxs]
+                for (trg_name, trg_value) in targets_per_image.get_fields().items():
+                    if trg_name.startswith("gt_") and not proposals_per_image.has(trg_name):
+                        proposals_per_image.set(trg_name, trg_value[sampled_targets])
+            proposals_per_image.set('sample_types',
+                                    torch.zeros_like(proposals_per_image.gt_classes).int())
+            if ann_type == 'only_caption':
+                proposals_per_image = proposals_per_image[:0]
+            proposals_per_image = Instances.cat([proposals_per_image, added_instances])
+            num_bg_samples.append((gt_classes == self.num_classes).sum().item())
+            num_fg_samples.append(gt_classes.numel() - num_bg_samples[-1])
+            proposals_with_gt.append(proposals_per_image)
+
+        # Log the number of fg/bg samples that are selected for training ROI heads
+        storage = get_event_storage()
+        storage.put_scalar("roi_head/num_fg_samples", np.mean(num_fg_samples))
+        storage.put_scalar("roi_head/num_bg_samples", np.mean(num_bg_samples))
+
+        return proposals_with_gt, group_infos
+
+    def _box_forward_train(self, box_features, proposals):
+        input_box_features = self.box_predictor.pre_forward(
+            box_features.mean(dim=[2, 3]))
+        pseudo_words = self.box_predictor.pred_words(input_box_features)
+        sample_types = torch.cat([p.sample_types for p in proposals], dim=0)
+        scores = self.box_predictor.pred_cls_score(pseudo_words[sample_types == 0])
+        proposal_deltas = self.box_predictor.bbox_pred(input_box_features[sample_types == 0])
+        predictions = dict(kd_pseudo_words=pseudo_words[sample_types == 1],
+                           caption_pseudo_words=pseudo_words[sample_types == 2],
+                           scores=scores,
+                           proposal_deltas=proposal_deltas)
+
+        return predictions
