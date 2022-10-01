ef2660a83dcdf55af990ccd97ed27dd551e87bfa
吴思泽 <wusize@wusizes-MacBook-Pro.local>
2022-10-01 14:14:01 +0800

    pre_process_json

---
+++ tools/pre_process_jsons/keep_base_classes.py
@@ -0,0 +1,28 @@
+import json
+import argparse
+import sys
+
+sys.path.insert(0, 'third_party/CenterNet2/')
+sys.path.insert(0, 'third_party/Deformable-DETR')
+from detic.data.datasets.coco_zeroshot import categories_seen
+
+base_cat_ids = [cat['id'] for cat in categories_seen]
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--json_path", default="datasets/coco/annotations/instances_val2017.json", type=str)
+parser.add_argument("--out_path", default="datasets/coco/wusize/instances_val2017_base.json")
+args = parser.parse_args()
+
+with open(args.json_path, 'r') as f:
+    json_coco = json.load(f)
+
+annotations = []
+
+for ann in json_coco['annotations']:
+    if ann['category_id'] in base_cat_ids:
+        annotations.append(ann)
+
+json_coco['annotations'] = annotations
+
+with open(args.out_path, 'w') as f:
+    json.dump(json_coco, f)
