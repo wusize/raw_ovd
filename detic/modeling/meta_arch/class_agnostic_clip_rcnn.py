ac42a8820bf37e50348d760860c335501d15f977
wusize <wusize@sensetime.com>
2022-10-14 12:53:52 +0800

    'class_agnostic'

--- detic/modeling/meta_arch/class_agnostic_clip_rcnn.py
+++ detic/modeling/meta_arch/class_agnostic_clip_rcnn.py
@@ -126,5 +126,6 @@ class CLassAgnosticCLIPRCNN(GeneralizedRCNN):
             box_centers = (boxes[:, 2:] + boxes[:, :2]) * 0.5
             x0y0s = (box_centers - box_whs * 0.5 * scalar).clamp(min=0.0)
             x1y1s = (box_centers + box_whs * 0.5 * scalar).clamp(max=torch.tensor([w, h]).to(boxes))
+            import pdb; pdb.set_trace()
             boxes_list.append(torch.cat([x0y0s, x1y1s], dim=-1))
         return boxes_list
