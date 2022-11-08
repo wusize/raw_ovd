841f95e903210c56834479219cafeccc47dfba1d
wusize <wusize@sensetime.com>
2022-11-08 11:06:37 +0800

    'eval_proposal'

--- detic/evaluation/custom_coco_eval.py
+++ detic/evaluation/custom_coco_eval.py
@@ -1,26 +1,13 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
-import contextlib
-import copy
-import io
 import itertools
-import json
-import logging
 import numpy as np
 import os
 import pickle
-from collections import OrderedDict
 import pycocotools.mask as mask_util
-import torch
-from pycocotools.coco import COCO
-from pycocotools.cocoeval import COCOeval
 from tabulate import tabulate
 
-import detectron2.utils.comm as comm
-from detectron2.config import CfgNode
-from detectron2.data import MetadataCatalog
-from detectron2.data.datasets.coco import convert_to_coco_json
-from detectron2.evaluation.coco_evaluation import COCOEvaluator
-from detectron2.structures import Boxes, BoxMode, pairwise_iou
+from detectron2.evaluation.coco_evaluation import COCOEvaluator, _evaluate_box_proposals
+from detectron2.structures import BoxMode
 from detectron2.utils.file_io import PathManager
 from detectron2.utils.logger import create_small_table
 from ..data.datasets.coco_zeroshot import categories_seen, categories_unseen
@@ -46,6 +33,46 @@ class WusizeCOCOEvaluator(COCOEvaluator):
                 prediction["proposals"] = output["proposals"].to(self._cpu_device)
             if len(prediction) > 1:
                 self._predictions.append(prediction)
+
+    def _eval_box_proposals(self, predictions):
+        """
+        Evaluate the box proposals in predictions.
+        Fill self._results with the metrics for "box_proposals" task.
+        """
+        if self._output_dir:
+            # Saving generated box proposals to file.
+            # Predicted box_proposals are in XYXY_ABS mode.
+            bbox_mode = BoxMode.XYXY_ABS.value
+            ids, boxes, objectness_logits = [], [], []
+            for prediction in predictions:
+                ids.append(prediction["image_id"])
+                boxes.append(prediction["proposals"].proposal_boxes.tensor.numpy())
+                objectness_logits.append(prediction["proposals"].objectness_logits.numpy())
+
+            proposal_data = {
+                "boxes": boxes,
+                "objectness_logits": objectness_logits,
+                "ids": ids,
+                "bbox_mode": bbox_mode,
+            }
+            with PathManager.open(os.path.join(self._output_dir, "box_proposals.pkl"), "wb") as f:
+                pickle.dump(proposal_data, f)
+
+        if not self._do_evaluation:
+            self._logger.info("Annotations are not available for evaluation.")
+            return
+
+        self._logger.info("Evaluating bbox proposals ...")
+        res = {}
+        areas = {"all": "", "small": "s", "medium": "m", "large": "l"}
+        for limit in [100, 1000]:
+            for area, suffix in areas.items():
+                stats = _evaluate_box_proposals(predictions, self._coco_api, area=area, limit=limit)
+                key = "AR{}@{:d}".format(suffix, limit)
+                res[key] = float(stats["ar"].item() * 100)
+                self._logger.info(f"All the evaluation result: {stats}")
+        self._logger.info("Proposal metrics: \n" + create_small_table(res))
+        self._results["box_proposals"] = res
 
 
 def instances_to_coco_json(instances, img_id):
